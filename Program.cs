a918ca0694ca94473ba436c1533d5c630369a8ce
PoptamasVlad <118806771+PoptamasVlad@users.noreply.github.com>
2022-11-25 21:14:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la tastatura.
+             Numarul are exact 2 cifre*/
+
+            int numar = int.Parse(Console.ReadLine());
+
+            int suma = numar % 10 + numar / 10;
+            Console.WriteLine(suma);
+        }
+    }
+}
